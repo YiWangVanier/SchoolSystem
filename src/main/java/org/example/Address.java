db097fe4d7fc1d561d8a0785914defc66c1c5602
YiWang-Vanier <wangy@vaniercollege.qc.ca>
2025-12-05 08:51:24 -0500

    Create the Address class

---
+++ src/main/java/org/example/Address.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Address {
+    private int streetNo;
+    private String street;
+    private String city;
+    private Province province;
+    private String zipCode;
+}
